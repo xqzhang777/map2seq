66c06cf418b74300ed46d328caf2e629dd31c786
xqzhang777 <105519489+xqzhang777@users.noreply.github.com>
2023-08-07 16:05:04 -0400

    deployment debug

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,8 @@ except ImportError:
    
     #st.info("downloading cctbx-base")
     ds = np.DataSource(tmpdir)
-    url_final = "https://drive.google.com/uc?export=download&id=1pWpLoyUOXqTbktqOJ24X5bdXa8u7lb0Y"
+    #url_final = "https://drive.google.com/uc?export=download&id=1pWpLoyUOXqTbktqOJ24X5bdXa8u7lb0Y"
+    url_final="https://drive.google.com/uc?export=1pMarQnGuABG9MRp9DW0TFY_hStakNF4S"
     if not ds.exists(url_final):
         print("download error")
     with ds.open(url_final) as fp:
