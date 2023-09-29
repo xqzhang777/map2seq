d5114477d89adc4eb6eade2617a2abf16a1670c5
xqzhang777 <105519489+xqzhang777@users.noreply.github.com>
2023-09-29 09:00:16 -0400

    Update fms_main.py

--- findmysequence_lib/findmysequence/fms_main.py
+++ findmysequence_lib/findmysequence/fms_main.py
@@ -295,8 +295,8 @@ def fms_run(mapin=None, modelin=None, seqin=None, modelout=None, slide=False, db
 #        parser.print_help()
 #        print
 #        return 1
-    
-    options=Fms_option(mapin=mapin, modelin=modelin, seqin=seqin, modelout=modelout, db=db, tmpdir=tmpdir, outdir=outdir, tophits=tophits,rev=rev,flip=flip, sld=slide)
+    sld123123=slide
+    options=Fms_option(mapin=mapin, modelin=modelin, seqin=seqin, modelout=modelout, db=db, tmpdir=tmpdir, outdir=outdir, tophits=tophits,rev=rev,flip=flip, sld=sld123123)
 
 #    if options.modelin is None:
 
